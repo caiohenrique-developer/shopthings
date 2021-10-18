fac3f072381d8d0a830ec71d068a6a8b69e558da
caiohenrique-developer <caiorussellrox@msn.com>
2021-10-18 13:13:27 -0300

    🎉 [feat]: Environment cfg

---
+++ .cz-config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  types: [
+    // My custom types
+    { value: '✨ [feat]', name: '✨ [feat]: A new feature' },
+    { value: '🎉 [feat]', name: '🎉 [initial]: Initial commit' },
+    { value: '✅ [fix]', name: '✅ [fix]: A bug fix' },
+    { value: '📝 [doc]', name: '📝 [doc]: Documentation only changes' },
+    {
+      value: '🎨 [style]',
+      name: '🎨 [style]: Changes that do not affect the meaning of the code\n (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: '♻️ [refactor]',
+      name: '♻️⠀⠀[refactor]: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: '🛸 [perf]',
+      name: '🛸 [perf]: A code change that improves performance',
+    },
+    { value: '🚨 [test]', name: '🚨 [test]: Adding missing tests' },
+    {
+      value: '🔨 [chore]',
+      name:
+        '🔨 [chore]: Changes to the build process or auxiliary tools\n and libraries such as documentation generation',
+    },
+    { value: '🔙 [revert]', name: '🔙 [revert]: Revert to a commit' },
+    { value: '📈 [wip]', name: '📈 [wip]: Work in progress' },
+
+    // To create release tags
+    { value: '🔖 [perf]', name: '🔖 { Major }: Add a release tag of perf type' },
+    { value: '🔖 [fix]', name: '🔖 { Patch }: Add a release tag of fix type' },
+    { value: '🔖 [feat]', name: '🔖 { Minor }: Add a release tag of feat type' },
+  ],
+
+  scopes: [{ name: '🔖' }, { name: 'release' }, { name: 'test' }, { name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+
+  // typePrefix: '[',
+  // typeSuffix: ']',
+  // subjectSeparator: ' '
+  upperCaseSubject: true
+}
