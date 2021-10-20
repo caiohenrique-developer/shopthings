2ab31ac294efa9b2b5f019b5653a55e88859c34b
caiohenrique-developer <caiorussellrox@msn.com>
2021-10-19 21:13:27 -0300

    ✨ [feat]: Add banner top for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import Head from 'next/head';
+import Image from 'next/image';
 
 import { Product } from '@components/Product';
 
 import { Container } from '@styles/home';
+
+import bannerTop from '../../public/assets/home-banner-top.png';
 
 export default function Home() {
   return (
@@ -14,6 +17,16 @@ export default function Home() {
       </Head>
 
       <Container className='app-container'>
+        <div>
+          <Image
+            src={bannerTop}
+            alt='Banner top from home page'
+            width={800}
+            height={534}
+            objectFit='cover'
+          />
+        </div>
+
         <div>
           <Product />
           <Product />
