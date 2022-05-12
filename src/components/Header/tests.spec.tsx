ead323c56a6a1100c183d32689fcb3af1ff28613
caiohenrique-developer <caiorussellrox@msn.com>
2022-05-12 14:03:26 -0300

    🚨 [test]: Should be able to click on the close responsive navigation menu button to close the navigation menu

--- src/components/Header/tests.spec.tsx
+++ src/components/Header/tests.spec.tsx
@@ -48,13 +48,25 @@ describe("check the user's header elements interactions handler", () => {
       .click();
   });
 
+  it('should not be able to click on my account & search disabled button', () => {
+    // find disabled buttons
+    cy.get('button[data-tst=my-account-btn]')
+      .should('exist')
+      .and('be.disabled');
+
+    cy.get('button[data-tst=search-for-btn]')
+      .should('exist')
+      .and('be.disabled');
+  });
+
+  // responsive tests
   context(`${tabletMaxWidth} x ${tabletMinWidth} viewport resolution`, () => {
     beforeEach(() => {
       // run these tests as if in a mobile or tablet device
       cy.viewport(tabletMaxWidth, tabletMinWidth);
     });
 
-    it('should be able to click on the responsive navigation menu button to open the navigation menu', () => {
+    it('should be able to click on the open responsive navigation menu button to open the navigation menu', () => {
       // mount the component in the DOM
       mount(
         <>
@@ -73,7 +85,7 @@ describe("check the user's header elements interactions handler", () => {
       // check if the navigation menu is not visible
       cy.get('nav[data-tst=responsive-navigation-menu]').should('not.exist');
 
-      // find and click on the responsive navigation menu button
+      // find and click on the open responsive navigation menu button
       cy.get('button[data-tst=burger-btn]')
         .should('exist')
         .and('not.be.disabled')
@@ -90,13 +102,28 @@ describe("check the user's header elements interactions handler", () => {
         .and('have.length', 7);
     });
 
-    it('should be able to click on the responsive navigation menu button to close the navigation menu', () => {
+    it('should be able to click on the close responsive navigation menu button to close the navigation menu', () => {
+      // mount the component in the DOM
+      mount(
+        <>
+          <Provider store={store}>
+            <PersistGate persistor={persistentStore}>
+              <CartOpenProvider>
+                <Header />
+              </CartOpenProvider>
+            </PersistGate>
+          </Provider>
+
+          <GlobalStyles />
+        </>,
+      );
+
       // check if the navigation menu is not visible
       cy.get('nav[data-tst=responsive-navigation-menu]').should('not.exist');
 
-      // find and click on the responsive navigation menu button
+      // find and click on the open responsive navigation menu button
       cy.get('button[data-tst=burger-btn]')
-        .should('be.visible')
+        .should('exist')
         .and('not.be.disabled')
         .click();
 
@@ -110,26 +137,15 @@ describe("check the user's header elements interactions handler", () => {
         .children('a')
         .and('have.length', 7);
 
-      // find and click on the responsive navigation menu button
+      // find and click on the close responsive navigation menu button
       cy.get('button[data-tst=burger-btn]')
-        .should('be.visible')
+        .should('exist')
         .and('not.be.disabled')
         .click();
 
       // check if the navigation menu is not visible
       cy.get('nav[data-tst=responsive-navigation-menu]').should('not.exist');
     });
-  });
-
-  it('should not be able to click on my account & search disabled button', () => {
-    // find disabled buttons
-    cy.get('button[data-tst=my-account-btn]')
-      .should('exist')
-      .and('be.disabled');
-
-    cy.get('button[data-tst=search-for-btn]')
-      .should('exist')
-      .and('be.disabled');
   });
 });
 
